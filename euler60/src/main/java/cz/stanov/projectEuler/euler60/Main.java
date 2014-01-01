55320ebf4a9c036cbfbf56022ea1e24308645c4d
Standa Novak <stanovec@gmail.com>
2014-01-01 17:10:23 +0100

    log4j added

--- euler60/src/main/java/cz/stanov/projectEuler/euler60/Main.java
+++ euler60/src/main/java/cz/stanov/projectEuler/euler60/Main.java
@@ -2,13 +2,21 @@ package cz.stanov.projectEuler.euler60;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author Stanislav Novak (stanislav.novak@ibacz.eu)
  */
 public class Main {
 
+    private static final Logger LOG = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
-        getResultForArgument(5);
+        LOG.debug("Starting computing!");
+
+        long result = getResultForArgument(5);
+
+        LOG.info("And the result is: " + result + "!!!");
     }
 
     public static long getResultForArgument(int i) {
