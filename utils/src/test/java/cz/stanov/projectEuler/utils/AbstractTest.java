6a4c267af7bd871fb274bab50e48219487f20c99
Standa Novak <stanovec@gmail.com>
2014-01-05 17:30:12 +0100

    added PalindromesTest

--- utils/src/test/java/cz/stanov/projectEuler/utils/AbstractTest.java
+++ utils/src/test/java/cz/stanov/projectEuler/utils/AbstractTest.java
@@ -1,6 +1,9 @@
 package cz.stanov.projectEuler.utils;
 
+import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,4 +13,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/utils-context.xml")
 public abstract class AbstractTest {
+
+    protected final Logger LOG = Logger.getLogger(this.getClass());
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+        LOG.debug("Mocks initialized.");
+    }
 }
