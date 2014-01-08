386ad763dbd4ffd88d8f3f31afc2b32d25b76069
Standa Novak <stanovec@gmail.com>
2014-01-08 22:11:48 +0100

    added SetGenerator and SetGeneratorFactory stubs

---
+++ utils/src/main/java/cz/stanov/projectEuler/utils/SetGeneratorFactory.java
@@ -0,0 +1,12 @@
+package cz.stanov.projecteuler.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Stanislav Novak (stanislav.novak@ibacz.eu)
+ */
+@Component
+public class SetGeneratorFactory {
+
+    //public
+}
